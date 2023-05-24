a39728c9bfb647a78c64f8528e92abeb59de45eb
Nick Crews <nicholas.b.crews@gmail.com>
2023-05-24 09:52:35 -0800

    Add env_logger crate to actually write log output

--- crates/feco3/src/lib.rs
+++ crates/feco3/src/lib.rs
@@ -12,6 +12,9 @@ pub fn add_42(a: i32) -> i32 {
 }
 
 pub fn print_header(path: &str) {
+    // TODO Figure out how to reconfigure this, since currently
+    // it only configures it on the first call and then never again.
+    env_logger::try_init();
     let file = File::open(path).unwrap();
     let mut parser = parser::Parser::from_reader(file);
     let header = parser.parse_header().unwrap();
