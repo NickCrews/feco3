4f45e8a72fad6a4b12d59a63282c4c28009c4635
Nick Crews <nicholas.b.crews@gmail.com>
2023-05-27 01:36:54 -0800

    Use pyo3_log, not env_logger

--- crates/feco3_python/src/lib.rs
+++ crates/feco3_python/src/lib.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 
 #[pyfunction]
 fn parse_from_path(fec_path: PathBuf, out_dir: PathBuf) -> PyResult<()> {
-    env_logger::try_init();
     match feco3::parse_from_path(&fec_path, out_dir) {
         Ok(()) => Ok(()),
         Err(e) => Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(e.to_string())),
@@ -12,6 +11,10 @@ fn parse_from_path(fec_path: PathBuf, out_dir: PathBuf) -> PyResult<()> {
 
 #[pymodule]
 fn _feco3(_py: Python, m: &PyModule) -> PyResult<()> {
+    // It is important to initialize the Python loggers first,
+    // before calling any Rust functions that may log.
+    // See https://pyo3.rs/v0.18.3/ecosystem/logging
+    pyo3_log::init();
     m.add_function(wrap_pyfunction!(parse_from_path, m)?)?;
     Ok(())
 }
