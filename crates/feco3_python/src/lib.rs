80faeb0c51f161eae28eac4df345865cc42dbd1e
Nick Crews <nicholas.b.crews@gmail.com>
2023-05-27 01:42:17 -0800

    Add FecFile pyclass to python API
    Still need a lot of other stuff

--- crates/feco3_python/src/lib.rs
+++ crates/feco3_python/src/lib.rs
@@ -1,6 +1,20 @@
 use pyo3::prelude::*;
 use std::path::PathBuf;
 
+#[pyclass]
+struct FecFile(feco3::FecFile);
+
+#[pymethods]
+impl FecFile {
+    #[staticmethod]
+    fn from_path(path: PathBuf) -> PyResult<Self> {
+        match feco3::FecFile::from_path(&path) {
+            Ok(fec_file) => Ok(FecFile(fec_file)),
+            Err(e) => Err(to_py_err(e)),
+        }
+    }
+}
+
 #[pyfunction]
 fn parse_from_path(fec_path: PathBuf, out_dir: PathBuf) -> PyResult<()> {
     match feco3::parse_from_path(&fec_path, out_dir) {
@@ -16,5 +30,18 @@ fn _feco3(_py: Python, m: &PyModule) -> PyResult<()> {
     // See https://pyo3.rs/v0.18.3/ecosystem/logging
     pyo3_log::init();
     m.add_function(wrap_pyfunction!(parse_from_path, m)?)?;
+    m.add_class::<FecFile>()?;
     Ok(())
+}
+
+fn to_py_err(e: feco3::Error) -> PyErr {
+    match e {
+        feco3::Error::HeaderParseError(e) => {
+            PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string())
+        }
+        feco3::Error::RecordParseError(e) => {
+            PyErr::new::<pyo3::exceptions::PyValueError, _>(e.to_string())
+        }
+        feco3::Error::IoError(e) => PyErr::new::<pyo3::exceptions::PyIOError, _>(e.to_string()),
+    }
 }
